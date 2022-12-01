75c2bc24134b3af69ad6b9b7634526fb660c87ac
leticea <nascimento.leticeah@gmail.com>
2022-12-01 11:05:22 -0300

    editando botões no css

--- script.js
+++ script.js
@@ -4,13 +4,16 @@ const handleFocus = ({ target }) => {
 
     const span = target.previousElementSibling;
     span.classList.add('span-active');
-}
+};
 
 const handleFocusOut = ({ target }) => {
 
-    const span = target.previousElementSibling;
-    span.classList.remove('span-active');
-}
+    if (target.value === '') {
+
+        const span = target.previousElementSibling;
+        span.classList.remove('span-active');
+    }
+};
 
 inputs.forEach((input) => input.addEventListener('focus', handleFocus));
 inputs.forEach((input) => input.addEventListener('focusout', handleFocusOut));
